35ab00ec2ae7d63cf23cbd09424f11e711304221
Dara adekore <adekore8@gmail.com>
2023-05-29 21:04:08 -0700

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Dara Adekore on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
