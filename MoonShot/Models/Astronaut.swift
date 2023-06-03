590daa2bb6f5fa3d4f86e280382dc78500843c05
Dara adekore <adekore8@gmail.com>
2023-06-03 16:16:10 -0700

    Initial commit

---
+++ MoonShot/Models/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Dara Adekore on 2023-05-29.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+    let id:String
+    let name:String
+    let description:String
+}
---
+++ MoonShot/Models/Colour-Theme.swift
@@ -0,0 +1,18 @@
+//
+//  Colour-Theme.swift
+//  MoonShot
+//
+//  Created by Dara Adekore on 2023-05-30.
+//
+
+import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red:0.1,green:0.1,blue:0.2)
+    }
+    static var lightBackground: Color {
+        Color(red:0.2,green:0.2,blue:0.3)
+    }
+}
